01ddc1d924f72c5ed0687888cac597e895135b8f
Pedro-Matos19 <pedrohenriquemo2020@gmail.com>
2026-02-25 14:54:52 -0300

    adicionando roteamento

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,33 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import ApplicationList from './pages/Application/ApplicationList';
+import CandidateForm from './pages/Candidate/CandidateForm';
+import CandidateList from './pages/Candidate/CandidateList';
+import CompanyForm from './pages/Company/CompanyForm';
+import CompanyList from './pages/Company/CompanyList';
+import CourseList from './pages/Course/CourseList';
+import Home from './pages/home/Home';
+import JobList from './pages/Job/JobList';
+import SkillList from './pages/Skill/SkillList';
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/candidates" element={<CandidateList />} />
+        <Route path="/candidates/new" element={<CandidateForm />} />
+        <Route path="/candidates/:id" element={<CandidateForm />} />
+        <Route path="/companies" element={<CompanyList />} />
+        <Route path="/companies/new" element={<CompanyForm />} />
+        <Route path="/companies/:id" element={<CompanyForm />} />
+        <Route path="/jobs" element={<JobList />} />
+        <Route path="/courses" element={<CourseList />} />
+        <Route path="/skills" element={<SkillList />} />
+        <Route path="/applications" element={<ApplicationList />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
